959e30a2dc19efc024f3fd75980fe97ba26ffd8d
Muhammed  Abdulhadi <mrglasswillbreak@gmail.com>
2026-09-13 22:20:49 +0100

    perf: trim interface fonts while retaining campus diacritics

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "@fontsource-variable/inter";
+import "./fonts.css";
 import "./styles.css";
 import App from "./App";
 createRoot(document.getElementById("root")!).render(
